3fe79d734b2adf63591823d75c16d9650593f009
nehasawant99 <nehasawant549@gmail.com>
2026-08-23 19:37:21 +0530

    refactor:Secure admin product management and user authentication

--- Pages/Admin/Products/Edit.cshtml.cs
+++ Pages/Admin/Products/Edit.cshtml.cs
@@ -1,5 +1,6 @@
 using GroceryShopping.Data;
 using GroceryShopping.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -7,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GroceryShopping.Pages.Admin.Products;
 
+[Authorize(Roles = "Admin")]
 public class EditModel : PageModel
 {
     private readonly ApplicationDbContext _context;
